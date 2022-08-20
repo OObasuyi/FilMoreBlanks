cfa1a2e28f9adc13633a0864e73b18ae9c98a6e7
oobasuyi <80005871+OObasuyi@users.noreply.github.com>
2022-08-21 01:25:36 +0200

    setup

--- setup.py
+++ setup.py
@@ -1,12 +1,25 @@
-from setuptools import setup
+import setuptools
 
-setup(
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
     name='FilMoreBlanks',
     version='v1.0.1',
-    packages=[''],
     url='https://github.com/OObasuyi/FilMoreBlanks/tree/master',
     license='MIT',
     author='osamuede obasuyi',
     author_email='eddieobasuyi@gmail.com',
-    description='CSV fill in the blank editor'
+    description='CSV fill in the blank editor',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),    # List of all python modules to be installed
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.8',
+    py_modules=["BlanketFill"],
+
 )

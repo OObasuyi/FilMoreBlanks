17419733758f93d64d4007e8518ea16d632b1acc
oobasuyi <80005871+OObasuyi@users.noreply.github.com>
2022-08-21 00:35:04 +0200

    init

---
+++ basic_access.py
@@ -0,0 +1,38 @@
+from argparse import ArgumentParser
+import utilites
+from blanket_fill import BlanketFill
+
+
+def access_fil():
+    parser = ArgumentParser(prog='FilMoreBlanks CSV filler')
+    mandatory_args = parser.add_argument_group(title='FilMoreBlanks Mandatory Fields')
+    mandatory_args.add_argument('-config_file', help='YAML config file for FilMoreBlanks', required=True, type=str)
+
+    args = parser.parse_args()
+
+    config_file = utilites.create_file_path(folder='FilMoreBlanks_Configs', file_name=args.config_file)
+    config_file = utilites.get_yaml_file(config_file)
+
+    # get CSV file
+    blank_it = BlanketFill(config_file['CSV_filename'])
+    # since it stored a dict, include the key as a tuple item as an argument.
+    for k,v in config_file['change_list'].items():
+        # type check
+        if not isinstance(v,list):
+            raise ValueError(f'THE RECEVIED VALUE IS NOT A LIST ITEM. you supplied a {type(v)}')
+
+        change_item = tuple([k,v])
+        # if BOTH keys match then we must only want to work on specific column(s)
+        for k_affect,only_affect in config_file['affect_only_columns'].items():
+            if k_affect == k:
+                only_affect = only_affect if isinstance(only_affect,list) else [only_affect]
+                blank_it.populate_csv(attr_to_change=change_item,selected_cols=only_affect)
+            else:
+                blank_it.populate_csv(attr_to_change=change_item)
+    # move file to spent and return new CSV.
+    utilites.move_spent_csv(config_file['CSV_filename'])
+    utilites.save_csv_data(blank_it.csv_data,f"COMPLETED_{config_file['CSV_filename']}")
+
+
+if __name__ == "__main__":
+    access_fil()
---
+++ blanket_fill.py
@@ -0,0 +1,65 @@
+import pandas as pd
+from utilites import create_file_path
+
+
+class BlanketFill:
+    def __init__(self,csv_name):
+        """ CSV must have features/col headers"""
+        self.csv_loc = create_file_path('digest',csv_name)
+        self.csv_data = pd.read_csv(self.csv_loc)
+
+    def populate_csv(self,attr_to_change:tuple, selected_cols:list='all'):
+        ''' old change such occupy first pos, new change second pos'''
+        if len(attr_to_change) != 2:
+            raise ValueError(f'attr_to_change value should only contain TWO values. We received {len(attr_to_change)}')
+
+        old_val, new_val = attr_to_change[0],attr_to_change[1]
+        new_val = new_val if isinstance(new_val,list) else [new_val]
+
+        if not isinstance(selected_cols, list):
+            if selected_cols == 'all':
+                selected_cols = self.csv_data.columns.tolist()
+            else:
+                selected_cols = [selected_cols]
+
+        new_data_collect = []
+        gather_old_idx = []
+        for idx in self.csv_data.index:
+            row_data = self.csv_data.loc[idx]
+            for col in selected_cols:
+                col_data = row_data[col]
+                if isinstance(col_data,float):
+                    try:
+                        col_data = str(int(col_data)).lstrip().rstrip()
+                    except Exception as error:
+                        continue
+                else:
+                    col_data = str(col_data).lstrip().rstrip()
+
+                if old_val != col_data:
+                    continue
+                else:
+                    gather_old_idx.append(idx)
+                    old_row = row_data.to_dict()
+                    for nv in new_val:
+                        new_row = old_row.copy()
+                        new_row[col] = nv
+                        new_data_collect.append(new_row)
+
+        self.csv_data.drop(gather_old_idx,inplace=True)
+        self.csv_data.reset_index(inplace=True,drop=True)
+        new_data = pd.DataFrame(new_data_collect)
+        self.csv_data = pd.concat([self.csv_data,new_data],ignore_index=True)
+        self.csv_data.drop_duplicates(inplace=True)
+
+
+if __name__ == "__main__":
+    bf = BlanketFill('test_csv.csv')
+    atc = ('new servers',['5,5,5,5','2.2.2.2','7,7,7,7'])
+    atc_2 = ('11.1.0.0/16',['233.31.244.39','100.19.68.193','235.253.243.128','232.151.66.30','74.22.1.140'])
+    atc_3 = ('169.156.158.231',['111.254.29.134','96.20.144.254'])
+    bf.populate_csv(atc)
+    bf.populate_csv(atc_2)
+    bf.populate_csv(atc_3,['source'])
+    bf.csv_data
+    g =1
